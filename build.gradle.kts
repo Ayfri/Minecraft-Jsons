f8262f84b9b90ddc5a55c803306801529449e35a
Ayfri <pierre.ayfri@gmail.com>
2022-04-19 06:23:59 +0200

    chore: Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,38 +3,38 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
-    id("org.jetbrains.compose") version "1.0.1"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.serialization") version "1.6.10"
+	id("org.jetbrains.compose") version "1.1.1"
 }
 
 group = "fr.ayfri"
 version = "1.0"
 
 repositories {
-    google()
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	google()
+	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    implementation(compose.desktop.currentOs)
-    implementation(compose.materialIconsExtended)
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+	implementation(compose.desktop.currentOs)
+	implementation(compose.materialIconsExtended)
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 	implementation(kotlin("reflect"))
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.jvmTarget = "1.8"
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "minecraft-jsons"
-            packageVersion = "1.0.0"
-        }
-    }
+	application {
+		mainClass = "MainKt"
+		nativeDistributions {
+			targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+			packageName = "minecraft-jsons"
+			packageVersion = "1.0.0"
+		}
+	}
 }
